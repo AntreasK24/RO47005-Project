3e9eccf196e163f76b7f4e9d8ce350d37bf64a2c
Mukil Saravanan <mukilsaravanan@tudelft.nl>
2024-12-03 11:48:15 +0100

    Remove the now ignored directory "build directories"

--- drone_ws/build/scenario_creator/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mukil/MSc_Robotics/Quarter2/Planning_&_Decision_Making_(RO47005)/Project/RO47005-Project/drone_ws/install/scenario_creator'
