a0c3ba0094949984ae552e6c3e104c4746e61fe5
Jonah <jeggenkemper@tudelft.nl>
2024-12-15 19:22:21 +0100

    Adding simulation creation script

---
+++ drone_ws/src/scenario_creator/scenario_creator/tests.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+def calculate_column_positions(foundation_length, foundation_width):
+
+    """
+    Calculate the positions of columns on a foundation.
+
+    Parameters:
+        foundation_length (float): Length of the foundation in meters.
+        foundation_width (float): Width of the foundation in meters.
+
+    Returns:
+        list: A list of (x, y) positions for the columns.
+    """
+   # Define parameters
+    margin = 1  # Distance from the edges to the center of the columns (meters)
+
+    # Compute the number of columns in each direction
+    num_columns_x = int((foundation_length - 2 * margin) / 4) + 1
+    num_columns_y = int((foundation_width - 2 * margin) / 4) + 1
+
+    # Adjust spacing to ensure uniform distribution
+    if num_columns_x > 1:
+        spacing_x = (foundation_length - 2 * margin) / (num_columns_x - 1)
+    else:
+        spacing_x = 0
+
+    if num_columns_y > 1:
+        spacing_y = (foundation_width - 2 * margin) / (num_columns_y - 1)
+    else:
+        spacing_y = 0
+
+    # Generate positions for columns
+    positions = []
+    for i in range(num_columns_x):
+        for j in range(num_columns_y):
+            x = margin + i * spacing_x
+            y = margin + j * spacing_y
+            positions.append((x, y))
+
+    return positions
+
+# Example usage:
+foundation_length = 14  # Length of the foundation (meters)
+foundation_width = 10   # Width of the foundation (meters)
+column_positions = calculate_column_positions(foundation_length, foundation_width)
+
+# Print results
+print("Column positions:")
+for pos in column_positions:
+    print(pos)
+    plt.scatter(pos[0],pos[1])
+#plt.scatter(column_positions)
+plt.show()
+
