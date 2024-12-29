e96e22993d885585587b94a34532a4fb06b8a560
Jonah <jeggenkemper@tudelft.nl>
2024-12-30 00:50:55 +0100

    Added basic ROS support for the environment creation

--- drone_ws/src/scenario_creator/scenario_creator/drone_scenario.py
+++ drone_ws/src/scenario_creator/scenario_creator/drone_scenario.py
@@ -1,21 +1,123 @@
-import rclpy
+
 import pybullet as p
 import pybullet_data
 import time
 import math
 import numpy as np
 
-
-'''
-import numpy as np
-import time
+# Adding ROS functionalities and drone interaction
+import rclpy
 from rclpy.node import Node
-
 from geometry_msgs.msg import Twist,Pose
 from gym_pybullet_drones.envs import VelocityAviary
 from gym_pybullet_drones.utils.enums import DroneModel, Physics
-'''
 
+
+
+
+class DroneSimulator(Node):  
+    #Constructor
+    def __init__(self):
+        super().__init__('drone_simulator')
+        #Subscriber
+        self.velocity_subscriber = self.create_subscription(Twist,'/cmd_vel',self.velocity_callback,10)
+        # TODO: self.waypoint_subscriber = self.create_subscription(Twist,'/cmd_vel',self.waypoint_callback,10)
+        #Publisher
+        self.pose_publisher = self.create_publisher(Pose, '/pose', 10)
+        # TODO self.static_obstacles_publisher = self.create_publisher(Pose, '/static_obstacles', 10)
+        # TODO timer_obstacles
+        # TODO self.dynamic_obstacles_publisher = self.create_publisher(Pose, '/dynamic_obstacles', 10)
+        
+        # Timer to publish and update dynamic obstacles
+        self.timer_period = 1.0/240.0
+        self.time = 0.0
+        self.timer = self.create_timer(self.timer_period,self.timer_simulation)
+
+        self.velocity_subscriber
+        self.env = VelocityAviary(drone_model=DroneModel.CF2X, num_drones=1, physics=Physics.PYB, ctrl_freq=240, gui=True)
+        self.obs = self.env.reset()  
+
+
+
+        ######## Obstacle and Environment Creation:
+        self.building = Building(
+            storeys=2,
+            position=[-2, -2, 0],
+            #storey_kwargs={
+            #    "column_kwargs": {"color": [1, 0, 0, 1], "radius": 0.3},
+            #    "ceiling_kwargs": {"color": [0.2, 0.2, 0.8, 0.6]},
+            #},
+        )
+        self.building.create()
+
+
+        self.dynamic_obstacle = Obstacle(color=[0,1,0,0.5], dynamic=True)
+        self.dynamic_obstacle.create()
+
+
+        # Visualize a path 
+        self.path_points = np.array([[0, 0, 0], [1, 1, 0.5], [2, 2, 1.5]])
+        self.path = PathVisual(self.path_points)
+
+
+
+    #Variables for controlling drone 
+    current_linear_velocity = np.array([[0.0, 0.0, 0.0, 1.0]])
+    current_angular_velocity = np.array([[0.0, 0.0, 0.0]])
+    current_velocity = np.hstack((current_linear_velocity,current_angular_velocity))
+
+    drone_position = None
+
+
+    def velocity_callback(self,msg):
+        #Get linear and angular velocity and stack them into a single vector
+        self.current_linear_velocity = np.array([[msg.linear.x,msg.linear.y,msg.linear.z,1]])
+        self.current_angular_velocity = np.array([[msg.angular.x,msg.angular.y,msg.angular.z]])
+        self.current_velocity = np.hstack((self.current_linear_velocity,self.current_angular_velocity))
+    
+    def waypoint_callback(self,msg):
+
+        pass
+        
+    def timer_obstacles(self):
+        pass
+
+    def timer_simulation(self):
+        p.stepSimulation()
+
+        ##### UPDATE obstacles
+        # Calculate new position using a sine wave for smooth movement
+        x_position = math.sin(self.time) * 2  # Oscillate between -2 and 2 along the x-axis
+        self.dynamic_obstacle.update_pose(position=[x_position, 0, 0.5],orientation=[0, 0, 0, 1])
+
+        new_waypoints = self.path_points + np.array([[0,0,0],[0,0,x_position*0.1],[0,0,x_position*0.1]])
+        new_waypoints[0,:] = self.drone_position
+        self.path.update(new_waypoints)
+
+
+        ##### UPDATE drone
+        #Give velocity commands to drone and publish position
+        pose_message = Pose()
+
+        obs, reward, done, truncated, info = self.env.step(self.current_velocity)
+
+        position = obs[0][0:3]
+        self.drone_position = position
+        pose_message.position.x = position[0]
+        pose_message.position.y = position[1]
+        pose_message.position.z = position[2]
+
+        quaternion = obs[0][3:7]
+        pose_message.orientation.x = quaternion[0]
+        pose_message.orientation.y = quaternion[1]
+        pose_message.orientation.z = quaternion[2]
+        pose_message.orientation.w = quaternion[3]
+
+        self.pose_publisher.publish(pose_message)
+
+        # TODO self.dynamic_obstacles_publisher.publish()
+
+        self.time += self.timer_period
 
 
 class Obstacle():
@@ -168,8 +270,10 @@ class Building():
             )
             storey.create()
 
+# TODO: Seperate Marker class to mark start and goal of the path
+
 class PathVisual:
-    def __init__(self, waypoints=None, line_color=[0, 1, 0], point_color=[1, 0, 0, 1], line_width=2, point_radius=0.05):
+    def __init__(self, waypoints=None, line_color=[0, 1, 0], point_color=[1, 0, 0, 1], line_width=2, point_radius=0.02):
         """
         This class is able to visualize planned waypoints in the simulation environment
         
@@ -293,6 +397,41 @@ class PathVisual:
 
 
 
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    drone_simulator = DroneSimulator()
+    rclpy.spin(drone_simulator)
+    drone_simulator.destroy_node()
+    rclpy.shutdown()
+
+
+
+if __name__ == '__main__':
+    main()
+
+# TODO: Make all the parameters from the lowlevel-class "Obstacle" available when calling "Building"
+
+# TODO: ROS params ,
+# TODO: randomization with random seed, 
+# TODO: dynamic, 
+# TODO: publish static obstacles ones, 
+# TODO: dynamic obstacles
+# TODO: publish dynamic obstacles,
+# TODO: publish types of obstacles 
+# TODO: input validation
+    # if not isinstance(value, int):
+    #    raise TypeError("Value must be an integer")
+    # if value <= 0:
+    #    raise ValueError("Value must be greater than zero")
+# TODO: subscribe to waypoint topic and draw waypoints with connecting edges to the screen
+
+
+### ROS MESSAGE shape_msgs/msg/SolidPrimitive Message https://docs.ros2.org/foxy/api/shape_msgs/msg/SolidPrimitive.html
+
+''' OLD non-ROS main loop: 
 def main(args=None):
         # Connect to PyBullet simulation
     physics_client = p.connect(p.GUI)  # Use p.DIRECT for non-GUI mode
@@ -344,40 +483,4 @@ def main(args=None):
         
 
     p.disconnect()
-
-
-
 '''
-
-def main(args=None):
-    rclpy.init(args=args)
-    drone_simulator = DroneSimulator()
-    rclpy.spin(drone_simulator)
-    drone_simulator.destroy_node()
-    rclpy.shutdown()
-'''
-
-
-
-if __name__ == '__main__':
-    main()
-
-# TODO: Make all the parameters from the lowlevel-class "Obstacle" available when calling "Building"
-
-# TODO: ROS params ,
-# TODO: randomization with random seed, 
-# TODO: dynamic, 
-# TODO: publish static obstacles ones, 
-# TODO: dynamic obstacles
-# TODO: publish dynamic obstacles,
-# TODO: publish types of obstacles 
-# TODO: input validation
-    # if not isinstance(value, int):
-    #    raise TypeError("Value must be an integer")
-    # if value <= 0:
-    #    raise ValueError("Value must be greater than zero")
-# TODO: subscribe to waypoint topic and draw waypoints with connecting edges to the screen
-
-
-### ROS MESSAGE shape_msgs/msg/SolidPrimitive Message https://docs.ros2.org/foxy/api/shape_msgs/msg/SolidPrimitive.html
-
