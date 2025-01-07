5ce4da09e5ec2d8842bc0245218ba423ac4001fc
Antreas Kourris <a.kourris@students.tudelft.nl>
2025-01-07 16:53:53 +0100

    Changed file structure

--- build/drone_mpc_python/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/antreas/MsC/Q2/RO47005/finalAssignment/RO47005-Project/install/drone_mpc_python'
